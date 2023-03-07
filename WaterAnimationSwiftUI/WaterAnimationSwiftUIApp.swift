4276a518b91b81b4d397f3fec03aca386d7c6908
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-03-07 20:26:22 +0200

    Initial Commit

---
+++ WaterAnimationSwiftUI/WaterAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAnimationSwiftUIApp.swift
+//  WaterAnimationSwiftUI
+//
+//  Created by Vladimir Dvornikov on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaterAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
